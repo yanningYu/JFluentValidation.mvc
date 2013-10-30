23133188105ada388475cb80913ca80da2706a11
yanningYu <yanning_carfield@hotmail.com>
2013-10-30 09:07:53 -0700

    Add NotNull.cshtml into project

---
+++ JFluentValidation/JFluentValidation/Controllers/JFluentValidationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JFluentValidation.Models;
+
+namespace JFluentValidation.Controllers
+{
+    public class JFluentValidationController : Controller
+    {
+        //
+        // GET: /JFluentValidation/
+
+        public ActionResult NotNull()
+        {
+            var viewModel = new AuthenticationModel();
+            return View(viewModel);
+        }
+
+    }
+}
