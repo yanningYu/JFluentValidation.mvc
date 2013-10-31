3210a7ecf6e55589ac0942268c588249831b208c
yanningYu <yanning_carfield@hotmail.com>
2013-10-31 06:50:43 -0700

    working on the second feature on NotNullWithCondition.js

---
+++ JFluentValidation/JFluentValidation/JFluentValidation/Scripts/NotNullWithCondition.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    $.fn.NotNullWithCondition = function (errorMsg, triggerSelector, triggerEvent, triggerfn) {
+        var selector = this;
+        triggerSelector.live(triggerEvent, function () {
+            alert(triggerfn);
+            if (triggerfn()) {
+                if (selector.valid()) {
+                    selector.each(function (index) {
+                        $(this).rules('add', {
+                            required: true,
+                            messages: {
+                                required: errorMsg
+                            }
+                        });
+                    });
+                }
+            } else {
+                selector.each(function (index) {
+                    $(this).rules("remove", "required");
+                });
+            }
+
+        });
+
+        return this;
+    };
+} (jQuery));
